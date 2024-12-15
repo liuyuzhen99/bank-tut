a6fff0eaa32a9c58068628750135ef1c5ce212c9
liuyuzhen99 <liuyuzhenbmjt@163.com>
2024-12-16 07:32:33 +0800

    first commit

---
+++ src/main/java/com/randy/banktut/BankTutApplication.java
@@ -0,0 +1,13 @@
+package com.randy.banktut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/randy/banktut/BankTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randy.banktut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
